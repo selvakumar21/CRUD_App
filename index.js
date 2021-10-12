79fb1ef3bd8b097d4401e354544b62f414d193b1
selvakumar21 <79460075+selvakumar21@users.noreply.github.com>
2021-10-12 07:11:39 +0530

    Add files via upload

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 let shortcutData = [];
+shortcutContents = document.getElementById("scRow");
 
 const addShortcut = () => {
     const newShortcut = {
@@ -8,38 +9,62 @@ const addShortcut = () => {
         pageurl: document.getElementById("pageURL").value
     };
 
-shortcutContents = document.getElementById("scRow");
-shortcutContents.insertAdjacentHTML('beforeend', createshortcut(newShortcut)); 
+
+shortcutContents.insertAdjacentHTML('beforeend', createShortcut(newShortcut)); 
 
 shortcutData.push(newShortcut);
 saveToLocalStorage();
 }
 
-const createshortcut = ({id, tag, imageurl, pageurl})
-    return(`<div class="col-md-6 col-lg-4 mt-3">
-    <div class="card bg-black" style="width: 9rem; height: 12rem">
+const createShortcut = ({id, tag, imageurl, pageurl}) => {
+    return(`<div class="col-md-6 col-lg-4 mt-3" id=${id} key=${id}>
+    <div class="card bg-black" style="width: 18rem;">
       <div class="card-header">
         <div class="d-flex justify-content-end">
-          <button type="button" class="btn btn-outline-light mb-3">
+          <button type="button" class="btn btn-outline-light">
             <i class="fa fa-pencil-alt"></i>
           </button>
-          <button type="button" class="btn btn-outline-light mb-3">
+          <button onclick="deleteShortcut(this)" type="button" name=${id} class="btn btn-outline-light">
             <i class="fa fa-trash"></i>
           </button>
         </div>
       </div>
-      <img style="border-radius: 100%" 
-        src="./assets/log.jfif"
+      <img
+        src="${imageurl}"
         class="card-img-top"
         alt="image"
+        style="color:white"
       />
       <div class="card-body">
-        <h5 class="card-name ">Name</h5>
+        <h6 class="card-title" style="color:white">${tag}</h6>
       </div>
       <div class="card-footer">
+      <a href="${pageurl}" target="_blank" >
         <button type="button" class="btn btn-outline-light float-end">
-          Open
-        </button>
+      Open
+    </button></a>
       </div>
     </div>
   </div>`)
+}
+
+const saveToLocalStorage = () => {
+  localStorage.setItem("shortcuts",JSON.stringify({shortcut: shortcutData}));
+}
+
+const reloadShortcut = () => {
+  const localStorageCopy = JSON.parse(localStorage.getItem("shortcuts"));
+  if(localStorageCopy) {
+    shortcutData = localStorageCopy["shortcut"]
+  }
+  shortcutData.map((data) =>{
+    shortcutContents.insertAdjacentHTML('beforeend', createShortcut(data));
+  })
+}
+
+const deleteShortcut = (e) => {
+  const targetID = e.getAttribute("name");
+  shortcutData = shortcutData.filter((data) => data.id!==targetID);
+  saveToLocalStorage();
+  window.location.reload();
+}
